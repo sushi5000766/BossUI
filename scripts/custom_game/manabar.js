3416df0322be6a4f173c369ef6c9f2d0c6f9fe97
Colin Hostetter <colinhostetter@gmail.com>
2016-01-16 12:42:18 -0500

    Fix an issue with the mana bar

--- scripts/custom_game/manabar.js
+++ scripts/custom_game/manabar.js
@@ -5,17 +5,22 @@ var label = $.GetContextPanel().FindChildrenWithClassTraverse("manabar-text")[0]
 var unit;
 
 function OnEntitySpawned(ev) {
+	$.Msg(ev)
+	$.Msg(Entities.GetUnitName(parseInt(ev.entindex)).substring(0,9))
 	if (Entities.GetUnitName(parseInt(ev.entindex)).substring(0,9) === "npc_boss_") {
 		SetManaBarOwner({entindex: ev.entindex});
 	}
 }
 
 function UpdateMana() {
-	if (unit) {
+	if (unit && Entities.IsAlive(unit)) {
 		var max = Entities.GetMaxMana(unit);
 		var current = Entities.GetMana(unit);
 		label.text = current + " / " + max;
 		fill.style.width = (current / max * 100).toString() + "%;";
+	} else {
+		label.text = "";
+		fill.style.width = "0%;";
 	}
 	$.Schedule(0.03, UpdateMana);
 }
@@ -30,7 +35,7 @@ function SetManaBarOwner(ev) {
 }
 
 (function() {
-	GameEvents.Subscribe("entity_spawned", OnEntitySpawned)
+	GameEvents.Subscribe("npc_spawned", OnEntitySpawned)
 	GameEvents.Subscribe("set_mana_bar_owner", SetManaBarOwner)
 	GameEvents.Subscribe("hide_mana_bar", HideManaBar)
 	UpdateMana();
